2755570660fba21f13f9e9860d51b912f5f364ad
Sudheer gupta <Sudheergupta0508@gmail.com>
2024-11-06 17:59:47 +0530

    No-return type required as  signature for the macro is changed to the of type void

--- src/hello_world.cc
+++ src/hello_world.cc
@@ -4,5 +4,5 @@ MY_NODE_MODULE_CALLBACK(SayMyName)
 {
   MY_NODE_MODULE_HANDLESCOPE
 
-  MY_NODE_MODULE_RETURN_VALUE(Napi::String::New(MY_NODE_MODULE_ENV, "Electron printer test"));
+  //MY_NODE_MODULE_RETURN_VALUE(Napi::String::New(MY_NODE_MODULE_ENV, "Electron printer test"));
 }
